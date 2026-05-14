483beae03f70357f74070b11411d806a8bde4786
Cloxt <ferdi.cloxt00@gmail.com>
2026-05-14 17:37:57 +0700

    fix: missing char script

--- includes/functions.php
+++ includes/functions.php
@@ -770,7 +770,6 @@ function generateMikrotikClientScript()
 
     # RADIUS
     $script .= "/radius incoming set accept=yes port=3799;\n";
-    $script .= "/radius incoming authentication-port=3799 accounting;\n";
     $script .= "/radius rem [find comment~\"".$appName."\"];\n";
     $script .= "/radius add address=".$OvpnIP." comment=\"".$appName."\" authentication-port=1812 accounting-port=1813 secret=\"".$radiusCredential['nas_secret']."\" service=ppp timeout=3s require-message-auth=no;\n";
 
