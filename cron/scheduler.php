75b36a23405d846ce475ab34583b284170506b51
D' Manuver <deer@cloxt.org>
2026-06-01 01:32:36 +0700

    fix: invoice status

--- cron/scheduler.php
+++ cron/scheduler.php
@@ -25,7 +25,7 @@ if (function_exists('date_default_timezone_set')) {
 if (php_sapi_name() === 'cli' && realpath(__FILE__) === realpath($_SERVER['SCRIPT_FILENAME'])) {
     runScheduler();
 } else if (php_sapi_name() !== 'cli' && realpath(__FILE__) === realpath($_SERVER['SCRIPT_FILENAME'])) {
-    die("This script can only be run from CLI or authorized web runner")ove;
+    die("This script can only be run from CLI or authorized web runner");
 }
 
 /**
@@ -213,7 +213,6 @@ function runAutoIsolir($pdo)
             AND i2.due_date < CURDATE()
         )
     ");
-
 
     echo "Found " . count($overdueInvoices) . " overdue invoices\n";
 
