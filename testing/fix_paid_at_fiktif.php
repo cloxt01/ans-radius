a216ff1678aa288f62929d96f280de2d0a00d395
D' Manuver <deer@cloxt.org>
2026-06-13 04:13:52 +0700

    fix: script fix paid_at fiktif invoice

--- testing/fix_paid_at_fiktif.php
+++ testing/fix_paid_at_fiktif.php
@@ -11,27 +11,22 @@ function generatePaidAtFromDistribution(string $paidAt): string
     $roll = rand(1, 100);
 
     if ($roll <= 32) {
-        // 09:00 - 11:00
         $start = 9 * 3600;
         $end   = 11 * 3600;
 
     } elseif ($roll <= 50) {
-        // 11:00 - 13:00
         $start = 11 * 3600;
         $end   = 13 * 3600;
 
     } elseif ($roll <= 70) {
-        // 13:00 - 15:00
         $start = 13 * 3600;
         $end   = 15 * 3600;
 
     } elseif ($roll <= 90) {
-        // 15:00 - 17:00
         $start = 15 * 3600;
         $end   = 17 * 3600;
 
     } else {
-        // 17:00 - 19:35
         $start = 17 * 3600;
         $end   = (19 * 3600) + (35 * 60);
     }
@@ -47,14 +42,14 @@ function generatePaidAtFromDistribution(string $paidAt): string
     return $date->format('Y-m-d H:i:s');
 }
 
-// ambil hanya invoice customer fiktif + jam rusak
+// ambil invoice fiktif dengan jam 00:00:00 - 04:00:00
 $sql = "
     SELECT i.id, i.paid_at
     FROM invoices i
     INNER JOIN fiktif_customers fc 
         ON fc.customer_id = i.customer_id
     WHERE i.paid_at IS NOT NULL
-      AND TIME(i.paid_at) <= '00:00:59'
+      AND TIME(i.paid_at) BETWEEN '00:00:00' AND '04:00:00'
 ";
 
 $rows = $pdo->query($sql)->fetchAll();
@@ -81,4 +76,4 @@ foreach ($rows as $row) {
 
 $pdo->commit();
 
-echo "DONE\n";
+echo "DONE repair 00:00–04:00 fiktif invoices\n";
